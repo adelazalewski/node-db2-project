3b8937c1e3845e6f6c51218de5ee2702bd027b89
adelazalewski <paliuadela@yahoo.com>
2020-12-21 01:44:31 -0500

    created table

--- migrations/20201221010957_magrations.js
+++ migrations/20201221010957_magrations.js
@@ -1,8 +1,15 @@
 
-exports.up = function(knex) {
-  
+exports.up =async function(knex) {
+  await knex.schema.createTable("cars", (table) => {
+      table.increments("id")
+      //the manufacturer code
+      table.float("manufacturer", 2).notNull().unique()
+      //5digit code whit all the car's info
+      table.float("model and milage", 5).notNull().unique()
+      table.text("type and status of title") //optional
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down =async function(knex) {
+  await knex.schema.dropTable("cars")
 };
